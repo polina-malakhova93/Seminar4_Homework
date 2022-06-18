001dbafec1153a068fb516591ec24afccc11fb77
polina993 <>
2022-06-18 18:55:48 +0300

    add task27

---
+++ task27/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+while( number > 0)
+{
+    int digit = number % 10;
+    sum +=digit;
+    number/=10;
+}
+Console.WriteLine(sum);
