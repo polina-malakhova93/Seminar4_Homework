d534ee162c8189a8e7683d4ddc4f7ce21a634e75
polina993 <>
2022-06-18 19:35:35 +0300

    add task29

---
+++ task29/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] array = new int [8];
+
+for(int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(0, 100);
+}
+PrintArray(array);
+
+void PrintArray( int[]array)
+{
+    int length = array.Length;
+    int index = 0;
+    while(index < length)
+    {
+        Console.Write(array[index] + " ");
+        index++;
+    }
+}
