0136196da78b2310b27a017503eede04b41ab36e
polina993 <>
2022-06-18 18:17:40 +0300

    add task25

---
+++ task25/Program.cs
@@ -0,0 +1,15 @@
+﻿int numberA = Input("Введите число А: ");
+int numberB = Input("Введите число В: ");
+int result = 1;
+
+for(int i = 1; i <= numberB; i++)
+{
+    result *=numberA;
+}
+Console.WriteLine(result);
+
+int Input(string output)
+{
+    Console.Write(output);
+    return Convert.ToInt32(Console.ReadLine());
+}
